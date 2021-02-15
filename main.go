4ee798419e4c623d495d40963ef5a4d6c0435c07
fedor <ahmedalbachiry@gmail.com>
2021-02-15 21:52:26 +0300

    adding some dialog with user for good experience

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ func main() {
 	}
 
 	cmd1 := cmds[1]
-	fmt.Println(cmd1)
 	if cmd1 != "retate" {
 		fmt.Println("\tthere is just \"retate\" command right now.\n\twe provid a mor usful commands soon")
 		os.Exit(1)
@@ -24,6 +23,27 @@ func main() {
 	}
 
 	photoName := cmds[2] // mybe flag is mor sinse
+	retated := ""
+	if len(cmds) == 4 {
+		retated = cmds[3]
+	}
+
+	if retated == "" {
+		for {
+			fmt.Println("\tthe result photo will take some name of source photo\n\tthe source photo will removed. Continue ? (Y/n) ? ")
+			var answr string
+			fmt.Scan(&answr)
+			if answr == "n" {
+				fmt.Println("Bye")
+				os.Exit(1)
+			} else if answr == "Y" || answr == "y" {
+				retated = photoName
+				break
+			} else {
+				fmt.Println("please read the message and type yes or no (Y/n)")
+			}
+		}
+	}
 
 	src, err := imaging.Open(photoName)
 	if err != nil {
@@ -32,7 +52,7 @@ func main() {
 	}
 	dst := imaging.Rotate90(src)
 
-	err = imaging.Save(dst, photoName)
+	err = imaging.Save(dst, retated)
 	if err != nil {
 		fmt.Printf("failed to save image: %v\n", err)
 	}
